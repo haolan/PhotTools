b3a9be96880eb42e2d882b6189c126de32814fc9
Haolan Zhao <haunteagle@gmail.com>
2014-10-02 15:48:09 +0200

    update

--- Demos.py
+++ Demos.py
@@ -1,3 +1,4 @@
 from PhotFunc import *
 
-print ps2nm(3, 800,0)
+print ps2nm(2, 800,0)
+print nm2tbp(0.3,800,3)
--- PhotFunc.py
+++ PhotFunc.py
@@ -8,10 +8,20 @@ def ps2nm(t,
     
     c = 299792458                   # in m/s
     
-    bandwidth = 0.441 * np.sqrt(1 + chirp**2) / t * 1e12 # in Hz
+    bandwidth = 0.32 * np.sqrt(1 + chirp**2) / t * 1e12 # in Hz
 
     if out == 'nm':
         return bandwidth * lambda0**2 / c * 1e-9 # in nm
 
     if out == 'cm-1':
         return bandwidth / c * 1e-2 # in cm^-1
+
+def nm2tbp(bandwidth_nm,
+           lambda0,
+           t):
+    
+    c = 299792458                   # in m/s
+    bandwidth_Hz = bandwidth_nm * c / lambda0**2 * 1e-3        #in THz
+    
+    return t * bandwidth_Hz
+    
